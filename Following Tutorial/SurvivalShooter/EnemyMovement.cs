c4475479882c1b45d2c988a575ddd3a9200c0871
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2020-06-12 13:23:12 +0200

    Add files via upload

---
+++ Following Tutorial/SurvivalShooter/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform Player;
+   PlayerHealth m_playerHealth;
+    EnemyHealth m_enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        Player = GameObject.FindGameObjectWithTag ("Player").transform;
+        m_playerHealth = Player.GetComponent <PlayerHealth> ();
+        m_enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> (); 
+    }
+
+
+    void Update ()
+    {
+        if(m_enemyHealth.currentHealth > 0 && m_playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination (Player.position);
+        }
+       else
+       {
+         nav.enabled = false;
+        }
+    }
+}
---
+++ Following Tutorial/SurvivalShooter/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float m_Speed = 6f;
+    Vector3 m_Movement;
+    Animator m_Anim;
+    Rigidbody m_PlayerRigidbody;
+    int floorMask;
+    float m_camRayLength = 100f;
+   
+
+
+   
+
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        m_Anim = GetComponent<Animator> ();
+        m_PlayerRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");//immediately at full speed when he starts moving.
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        m_Movement.Set(h, 0f, v);
+        m_Movement = m_Movement.normalized * m_Speed * Time.deltaTime;
+
+        m_PlayerRigidbody.MovePosition(transform.position + m_Movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Floorhit;
+
+        if (Physics.Raycast(camRay, out Floorhit, m_camRayLength, floorMask))
+        {
+            Vector3 playertoMouse = Floorhit.point - transform.position;
+            playertoMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playertoMouse);
+            m_PlayerRigidbody.MoveRotation(newRotation);
+
+        }
+
+    }
+
+    void Animating (float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        m_Anim.SetBool("IsWalking", walking);
+
+
+    }
+
+}
