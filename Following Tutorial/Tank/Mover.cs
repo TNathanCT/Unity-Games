6db4c9a43dcaeab32195f5151107bdf4ea04beda
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2020-06-12 13:21:56 +0200

    Add files via upload

---
+++ Following Tutorial/Tank/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+    private Rigidbody RB;
+
+	void Start() {
+        RB = GetComponent<Rigidbody>();
+		RB.velocity = transform.forward * speed;
+	}
+}
---
+++ Following Tutorial/Tank/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	public float tumble;
+    private Rigidbody RB;
+
+
+	void Start() {
+        RB = GetComponent<Rigidbody>();
+		RB.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
